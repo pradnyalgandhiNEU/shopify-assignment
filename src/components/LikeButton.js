94a8906899def3ba8cf713cc3b8a36469efd44e2
pradnyalgandhi <pradnyal.gandhi@sakec.ac.in>
2022-01-13 03:30:48 -0500

    Final

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import classes from './LikeButton.module.css';
 import Heart from "react-animated-heart";
 
 const LikeButton = props =>{
     const [isClick, setClick] = useState(false);
     
     return(
-        <Heart isClick={isClick} onClick={() => setClick(!isClick)}/>
+        <div className={classes.like}>
+            <h3>LEAVE A LIKE:</h3>
+            <Heart isClick={isClick} onClick={() => setClick(!isClick)}/>
+        </div>
+        
     )
 };
 
